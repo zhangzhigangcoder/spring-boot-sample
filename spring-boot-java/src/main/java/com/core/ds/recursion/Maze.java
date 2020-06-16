70b5455bdedd1926e479e54e891891d4f3c22e00
zhangzhigang <zhangzhigang_tech@163.com>
2020-06-16 17:17:57 +0800

    ！

---
+++ spring-boot-java/src/main/java/com/core/ds/recursion/Maze.java
@@ -0,0 +1,100 @@
+package com.core.ds.recursion;
+
+/**
+ * 使用递归解决迷宫问题
+ *
+ */
+public class Maze {
+
+	public static void main(String[] args) {
+		int rows = 7, cols = 6;
+		int[][] map = initMap(rows, cols);
+		System.out.println("地图详情：");
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+		long start = System.currentTimeMillis();
+		boolean result = findWay(map, 1, 1, rows - 2, cols - 2);
+		System.out.println(result);
+		System.out.println(System.currentTimeMillis() - start);
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	/**
+	 * 走迷宫
+	 * 游戏规则：
+	 * 1.  起始点(i,j)为(1,1)
+	 * 2. 如果能走到(rows -2, cols -2),就算成功
+	 * 3.对map[i][j]进行约定：
+	 * 		0：没有走过
+	 * 		1：墙
+	 * 		2：已经走过，是通路
+	 * 		3：已经走过，但是走不通 
+	 * @param map 地图
+	 * @param (startRow, startCol) 起始点
+	 * @param (endRow, endCol) 出口
+	 * @return
+	 */
+	public static boolean findWay(int[][] map, int startRow, int startCol, int endRow, int endCol) {
+		if (map[endRow][endCol] == 2) { // 已经找到
+			return true;
+		} else {
+			if (map[startRow][startCol] == 0) { // 该点还没有走过
+				map[startRow][startCol] = 2; // 假定该点是可以走通的
+				if (findWay(map, startRow + 1, startCol, endRow, endCol)) { // 向下走
+					return true;
+				} else if (findWay(map, startRow, startCol + 1, endRow, endCol)) { // 向右走
+					return true;
+				} else if (findWay(map, startRow - 1, startCol, endRow, endCol)) { // 向上走
+					return true;
+				} else if (findWay(map, startRow, startCol - 1, endRow, endCol)) { // 向左走
+					return true;
+				} else {
+					// 4个方向都没有走通，将该点标记为3
+					map[startRow][startCol] = 3;
+					return false;
+				}
+			} else { // 如果map[startRow][startCol] != 0, 可能是 1, 2, 3
+				return false;
+			}
+		}
+	}
+	
+	/**
+	 * 初始化地图
+	 * @param rows 行数
+	 * @param cols 列数
+	 * @return
+	 */
+	public static int[][] initMap(int rows, int cols) {
+		int[][] map = new int[rows][cols];
+		// 上下全部置为1
+		for (int i = 0; i < cols; i++) {
+			map[0][i] = 1;
+			map[rows-1][i] = 1;
+		}
+		
+		// 左右全部置为1
+		for (int i = 0; i < rows; i++) {
+			map[i][0] = 1;
+			map[i][cols-1] = 1;
+		}
+		
+		// 生成挡板
+		map[5][1] = 1;
+		map[3][2] = 1;
+		map[4][3] = 1;
+		map[5][2] = 1;
+		return map;
+	}
+	
+	
+}
--- spring-boot-java/src/main/java/com/core/ds/stack/PolandNotation.java
+++ spring-boot-java/src/main/java/com/core/ds/stack/PolandNotation.java
@@ -1,10 +1,9 @@
 package com.core.ds.stack;
 
-import static org.hamcrest.CoreMatchers.containsString;
-
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -13,40 +12,25 @@ import java.util.Stack;
  */
 public class PolandNotation {
 
-	public static void main(String[] args) {
-//		// (3+4)*5/35-6 -> "3 4 + 5 * 35 / 6 -"
-//		String suffixExpression = "3 4 + 5 * 35 / 6 -";
-//		int res = calculate(suffixExpression);
-//		System.out.println(res);
-		List<String> list = new ArrayList<String>();
-		String s = "2+(3+4)*5/35-61";
-		String[] str = 	s.split("\\d+");
-		int index = 0;
-		for (int i = 0; i < str.length - 1; i++) {
-			if ("".equals(str[i])) {
-				continue;
-			}
-			int idx1 = s.indexOf(str[i], index);
-			int idx2 = s.indexOf(str[i+1], idx1 + 1);
-			if (list.size() == 0 && idx1 != 0) {
-				list.add(s.substring(0, idx1));
-			}
-			for (char ch : str[i].toCharArray()) {
-				list.add(String.valueOf(ch));
-			}
-			list.add(s.substring(idx1 + str[i].length(), idx2));
-			if (i == str.length - 2) {
-				for (char ch : str[i+1].toCharArray()) {
-					list.add(String.valueOf(ch));
-				}
-				list.add(s.substring(idx2 + str[i+1].length(), s.length()));
-			}
-			index = idx1 + str[i].length();
+	// 运算符优先级定义
+	private static Map<String, Integer> operations = new HashMap<String, Integer>() {
+		private static final long serialVersionUID = 3082911361946594649L;
+
+		{
+			put("(", -1);
+			put("+", 1);
+			put("-", 1);
+			put("*", 2);
+			put("/", 2);
 		}
-		list.forEach(e -> {
-			System.out.println(e);
-		});
-		
+	};
+	
+	public static void main(String[] args) {
+		List<String> list = parseToSuffixExpressionList("2+(3+4)*5/35-6");
+		list.forEach(e -> System.out.print(e + "\t"));
+		int res = calculate(list);
+		System.out.println();
+		System.out.println("计算结果:" + res);
 	}
 	
 	/**
@@ -54,10 +38,9 @@ public class PolandNotation {
 	 * @param suffixExpression
 	 * @return
 	 */
-	public static int calculate(String  suffixExpression) {
-		List<String> list = Arrays.asList(suffixExpression.split(" "));
+	public static int calculate(List<String>  suffixExpressList) {
 		Stack<String> stack = new Stack<>();
-		list.forEach(e -> {
+		suffixExpressList.forEach(e -> {
 			if (e.matches("\\d+")) { // 匹配数字
 				stack.push(e);
 			} else {
@@ -99,6 +82,76 @@ public class PolandNotation {
 		return res;
 	}
 	
+	/**
+	 * 将表达式转换成List
+	 * @param expression
+	 * @return
+	 */
+	private static List<String> parseToList(String expression) {
+		List<String> list = new ArrayList<String>();
+		String[] str = 	expression.split("\\d+");
+		int index = 0, idx1 = 0, idx2 = 0;
+		for (int i = 0; i < str.length - 1; i++) {
+			if ("".equals(str[i])) {
+				continue;
+			}
+			idx1 = expression.indexOf(str[i], index);
+			idx2 = expression.indexOf(str[i+1], idx1 + 1);
+			if (list.size() == 0 && idx1 != 0) {
+				list.add(expression.substring(0, idx1));
+			}
+			for (char ch : str[i].toCharArray()) {
+				list.add(String.valueOf(ch));
+			}
+			list.add(expression.substring(idx1 + str[i].length(), idx2));
+			if (i == str.length - 2) {
+				for (char ch : str[i+1].toCharArray()) {
+					list.add(String.valueOf(ch));
+				}
+				list.add(expression.substring(idx2 + str[i+1].length(), expression.length()));
+			}
+			index = idx1 + str[i].length();
+		}
+		return list;
+	}
 	
-	
+	/**
+	 * 将中缀表达式转换成List
+	 * @param expression
+	 * @return
+	 */
+	public static List<String> parseToSuffixExpressionList(String expression) {
+		List<String> expressList = parseToList(expression);
+		Stack<String> operStack = new Stack<>(); // 符号栈
+		List<String> result = new ArrayList<>(); // 存放中间结果
+		for (String item : expressList) {
+			// 判断类型
+			if (item.matches("\\d+")) { 
+				// 是数字，将其压入ls
+				result.add(item);
+			} else if (item.equals("(")) { 
+				// 是左括号，则直接压入operStack
+				operStack.push(item);
+			} else if (item.equals(")")) { // 是右括号
+				// 是右括号")",则依次弹出s1栈顶的运算符，并压入ls，直到遇到左括号为止，此时将这一对括号丢弃
+				while (!operStack.peek().equals("(")) {
+					result.add(operStack.pop());
+				}
+				operStack.pop(); // 将"("弹出operStack栈，即清除小括号
+			} else {
+				// 是运算符,当优先级小于等于operStack栈顶运算符时，将operStack栈顶运算符弹出并加入ls中
+				// 再次转到d1与operStack栈中新的栈顶运算符相比较
+				while (operStack.size() != 0 && operations.get(item) <= operations.get(operStack.peek())) {
+					result.add(operStack.pop());
+				}
+				// 否则直接压入(operStack为空 || 栈顶为"(" || item优先级比栈顶高)
+				operStack.push(item);
+			}
+		}
+		// 把operStack中剩余的运算符依次弹出并加入ls中
+		while (operStack.size() != 0) {
+			result.add(operStack.pop());
+		}
+		return result;
+	}
 }
