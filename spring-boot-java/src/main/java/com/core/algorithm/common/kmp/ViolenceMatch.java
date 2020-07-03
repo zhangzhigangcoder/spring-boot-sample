bf56f25720b9caa5ab6779e76059eb433a59f0ec
zhangzhigang <zhangzhigang_tech@163.com>
2020-07-03 17:24:45 +0800

    !

--- spring-boot-java/src/main/java/com/core/algorithm/common/kmp/ViolenceMatch.java
+++ spring-boot-java/src/main/java/com/core/algorithm/common/kmp/ViolenceMatch.java
@@ -7,12 +7,12 @@ package com.core.algorithm.common.kmp;
 public class ViolenceMatch {
 
 	public static void main(String[] args) {
-	 String str1 = "硅硅谷 尚硅谷你尚硅 尚硅谷你尚硅谷你尚硅你好";
-	 String str2 = "尚硅谷你尚硅你";
-	 System.out.println(violenceMatch(str1, str2));
+		String str1 = "硅硅谷 尚硅谷你尚硅 尚硅谷你尚硅谷你尚硅你好";
+		String str2 = "尚硅谷你尚硅你";
+		System.out.println(violenceMatch(str1, str2));
 	}
-	
-	public static  int violenceMatch(String str1, String str2) {
+
+	public static int violenceMatch(String str1, String str2) {
 		char[] s1 = str1.toCharArray();
 		char[] s2 = str2.toCharArray();
 		int len1 = s1.length, len2 = s2.length;
@@ -31,5 +31,5 @@ public class ViolenceMatch {
 		}
 		return -1;
 	}
-	
+
 }
