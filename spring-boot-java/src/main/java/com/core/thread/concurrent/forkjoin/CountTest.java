edb1f42f735b888c0f5c68135a5f7d7b90afc9a0
zhangzhigang <zhangzhigang@kedacom.com>
2021-06-24 09:26:19 +0800

    添加kafka demo

--- spring-boot-java/src/main/java/com/core/thread/concurrent/forkjoin/CountTest.java
+++ spring-boot-java/src/main/java/com/core/thread/concurrent/forkjoin/CountTest.java
@@ -1,5 +1,6 @@
 package com.core.thread.concurrent.forkjoin;
 
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
@@ -7,42 +8,68 @@ import java.util.concurrent.RecursiveTask;
 
 public class CountTest {
 
-	static final class SumTask extends RecursiveTask<Integer> {
-		private static final long serialVersionUID = 1L;
-		
-		final int start; //开始计算的数
-		final int end; //最后计算的数
-		
-		SumTask(int start, int end) {
-			this.start = start;
-			this.end = end;
-		}
+    public static void main(String[] args) throws Exception {
+        // 创建2000个随机数组成的数组:
+        long[] array = new long[2000];
+        long expectedSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random();
+            expectedSum += array[i];
+        }
+        System.out.println("Expected sum: " + expectedSum);
+        // fork/join:
+        ForkJoinTask<Long> task = new SumTask(array, 0, array.length);
+        long startTime = System.currentTimeMillis();
+        Long result = ForkJoinPool.commonPool().invoke(task);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Fork/join sum: " + result + " in " + (endTime - startTime) + " ms.");
+    }
 
-		@Override
-		protected Integer compute() {
-			//如果计算量小于1000，那么分配一个线程执行if中的代码块，并返回执行结果
-			if(end - start < 1000) {
-				System.out.println(Thread.currentThread().getName() + " 开始执行: " + start + "-" + end);
-				int sum = 0;
-				for(int i = start; i <= end; i++)
-					sum += i;
-				return sum;
-			}
-			//如果计算量大于1000，那么拆分为两个任务
-			SumTask task1 = new SumTask(start, (start + end) / 2);
-			SumTask task2 = new SumTask((start + end) / 2 + 1, end);
-			//执行任务
-			task1.fork();
-			task2.fork();
-			//获取任务执行的结果
-			return task1.join() + task2.join();
-		}
-	}
-	
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		ForkJoinPool pool = new ForkJoinPool();
-		ForkJoinTask<Integer> task = new SumTask(1, 10000);
-		pool.submit(task);
-		System.out.println(task.get());
-	}
+    static Random random = new Random(0);
+
+    static long random() {
+        return random.nextInt(10000);
+    }
+}
+
+class SumTask extends RecursiveTask<Long> {
+    static final int THRESHOLD = 500;
+    long[] array;
+    int start;
+    int end;
+
+    SumTask(long[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        if (end - start <= THRESHOLD) {
+            // 如果任务足够小,直接计算:
+            long sum = 0;
+            for (int i = start; i < end; i++) {
+                sum += this.array[i];
+                // 故意放慢计算速度:
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                }
+            }
+            return sum;
+        }
+        // 任务太大,一分为二:
+        int middle = (end + start) / 2;
+        System.out.println(String.format("split %d~%d ==> %d~%d, %d~%d", start, end, start, middle, middle, end));
+        SumTask subtask1 = new SumTask(this.array, start, middle);
+        SumTask subtask2 = new SumTask(this.array, middle, end);
+        invokeAll(subtask1, subtask2);
+        Long subresult1 = subtask1.join();
+        Long subresult2 = subtask2.join();
+        Long result = subresult1 + subresult2;
+        System.out.println("result = " + subresult1 + " + " + subresult2 + " ==> " + result);
+        return result;
+    }
+
 }
